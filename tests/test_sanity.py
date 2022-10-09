c8892ceecb6b48b3a3b0499ed071b38cce85ce83
Sam Thorold <sam.thorold@gmail.com>
2022-10-09 15:35:43 +0100

    Initial tests

---
+++ tests/test_sanity.py
@@ -0,0 +1,13 @@
+from main import Card, Deck, Face, Suite
+
+
+def test_sanity():
+    deck = Deck(seed=1)
+    exp = [
+        Card(suite=Suite.CLUB, face=Face.TWO),
+        Card(suite=Suite.CLUB, face=Face.FIVE),
+    ]
+    got = [deck.draw()]
+    deck.shuffle()
+    got.append(deck.draw())
+    assert got == exp
